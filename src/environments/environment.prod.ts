43433489b9d4f94ba480899bb49f0dfb8b580aba
Miguel <miguel@ParrotSoft.local>
2019-05-29 22:12:06 -0500

    Agregando imagenes, logo y splashç

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  parse_app_id: 'EuFQw7E1yYJd2MkEsLcZiOZrLQZh8sFUZjSolFAF',
+  parse_server_url: 'https://parseapi.back4app.com/',
+  parse_js_key: '03YfHaJNRoysvjoQWFfgJyU34DbyhcfqMk33zmnI',
+  TalkappId: 'ty0IXMvO'
 };
